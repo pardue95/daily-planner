a968e2df0a035f0eb1e53c302e1f8e4b0ac540ca
pardue95 <cwpardue@gmail.com>
2021-10-19 16:48:43 -0500

    add html for time blocks

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,6 +2,17 @@ var getCurrentDate = function() {
     var todaysDate = moment().format('dddd, MMMM Do')
     console.log(todaysDate)
     $("#currentDay").text(todaysDate)
-};
 
+  
+};
+// store hour and text in local storage
+//retrieve hour and text from local storage
 getCurrentDate();
+
+$("hour-block").each(function(index){
+    console.log(index)
+})
+var hour = document.getElementById(".hour-block")
+
+console.log(hour)
+localStorage.setItem('hour', 'text');
