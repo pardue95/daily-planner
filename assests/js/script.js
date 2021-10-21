7013f58b4a9bb8c11aeaaa17365e9aa12d3ea183
pardue95 <cwpardue@gmail.com>
2021-10-21 12:15:34 -0500

    added local storage

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,16 +3,58 @@ var getCurrentDate = function() {
     console.log(todaysDate)
     $("#currentDay").text(todaysDate)
 
+    //retrieve hour and text from local storage
+$(".hour-block").each(function() {
+    var id = $(this).attr("id");
+    var text = localStorage.getItem(id)
+
+    if (text !== null) {
+        $(this).children(".text").val(text);
+    }
+})
+
   
 };
 // store hour and text in local storage
 //retrieve hour and text from local storage
 getCurrentDate();
 
-$("hour-block").each(function(index){
-    console.log(index)
-})
-var hour = document.getElementById(".hour-block")
+// store hour and text in local storage
+var saveBtn = $(".saveBtn")
 
-console.log(hour)
-localStorage.setItem('hour', 'text');
+$(".saveBtn").click(function(){
+    var hour = $(this).parent().attr("id")
+    console.log(hour)
+    var text = $(this).siblings(".text").val()
+    console.log(text)
+    localStorage.setItem(hour, text);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     function changeColor (){
+
+// var hour = document.getElementById(".hour-block")
+// $(hour).each(function(index){
+//     console.log(index)
+// })
+// }
+// var hour = document.getElementById(".hour-block")
+
+// console.log(hour)
+// localStorage.setItem('hour', 'text');
+
+// changeColor()
