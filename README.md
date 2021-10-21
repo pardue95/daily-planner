939dd3d379e32d6bbc9dd4cac9e1debeb89039db
pardue95 <cwpardue@gmail.com>
2021-10-21 13:22:43 -0500

    update README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Work Day Scheduler Starter Code
+## Work Day Scheduler 
+
+This project utilizes jQuery, Bootstrap and Moment.JS.  This daily planner allow a user to keep track pf task throught out the day. Local storage was implemented so that task would persist thru a page refresh or browser closing.
+
+The daily planner uses visual color indicators to show whether a task is in the past, present, or future.
+
+https://pardue95.github.io/daily-planner/
+
+https://github.com/pardue95/daily-planner
+
+![image](https://user-images.githubusercontent.com/85760640/138335061-15eafdd3-0cfc-437a-a675-f8a4441f44ac.png)
--- assests/js/script.js
+++ assests/js/script.js
@@ -1,60 +1,56 @@
 // Puts date in header
-var getCurrentDate = function() {
-    var todaysDate = moment().format('dddd, MMMM Do')
-    console.log(todaysDate)
-    $("#currentDay").text(todaysDate)
+var getCurrentDate = function () {
+    var todaysDate = moment().format("dddd, MMMM Do");
+    console.log(todaysDate);
+    $("#currentDay").text(todaysDate);
 
     //retrieve hour and text from local storage
-$(".hour-block").each(function() {
-    var id = $(this).attr("id");
-    var text = localStorage.getItem(id)
+    $(".hour-block").each(function () {
+        var id = $(this).attr("id");
+        var text = localStorage.getItem(id);
 
-    if (text !== null) {
-        $(this).children(".text").val(text);
-    }
-})
-
-  
+        if (text !== null) {
+            $(this).children(".text").val(text);
+        }
+    });
 };
 
 getCurrentDate();
 
-// store hour and text in local storage
-var saveBtn = $(".saveBtn")
-// captures click to save text
 
-$(".saveBtn").click(function(){
-    var hour = $(this).parent().attr("id")
-    console.log(hour)
-    var text = $(this).siblings(".text").val()
-    console.log(text)
+var saveBtn = $(".saveBtn");
+// captures click to save text
+// store hour and text in local storage
+$(".saveBtn").click(function () {
+    var hour = $(this).parent().attr("id");
+    console.log(hour);
+    var text = $(this).siblings(".text").val();
+    console.log(text);
     localStorage.setItem(hour, text);
-})
+});
 
 // Indicate whether it is past, present or future
 
-function pastPresentFuture () {
+function pastPresentFuture() {
     // check if index is equal to, less than , or greater than the current hour
- var currentHour = moment().hour();
-//itereate thru hours bock to check index
+    var currentHour = moment().hour();
+    //itereate thru hours bock to check index
 
-$(".hour-block").each(function (){
-//change ID to an integer
-var compareHour = parseInt($(this).attr("id"))
- console.log(compareHour)
-    if (compareHour < currentHour) {
-        $(this).addClass("past")
-    } else if ( compareHour === currentHour) {
-        $(this).addClass("present")
-    } else {
-        $(this).addClass("future")
-    }
-})}
+    $(".hour-block").each(function () {
+        //change ID to an integer
+        var compareHour = parseInt($(this).attr("id"));
+        console.log(compareHour);
+        if (compareHour < currentHour) {
+            $(this).addClass("past");
+        } else if (compareHour === currentHour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    });
+}
 
-
-
-pastPresentFuture()
-
+pastPresentFuture();
 
 
 
@@ -65,16 +61,3 @@ pastPresentFuture()
 
 
 
-//     function changeColor (){
-
-// var hour = document.getElementById(".hour-block")
-// $(hour).each(function(index){
-//     console.log(index)
-// })
-// }
-// var hour = document.getElementById(".hour-block")
-
-// console.log(hour)
-// localStorage.setItem('hour', 'text');
-
-// changeColor()
